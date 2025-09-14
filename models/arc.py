ff95820507a722caaaf17a61bd0d1529d4e82a4a
MarianoVidal <marianobazalarvidal@gmail.com>
2025-09-14 19:08:45 -0300

    Corrección: le faltaban los dos puntos a cada método de la clase

--- models/arc.py
+++ models/arc.py
@@ -3,12 +3,12 @@
 class Arc:
     """ Los arcos pueden tener nombre y 'multiplicidad' (denominación provisoria) """
 
-    def __init__(self, name = "")
+    def __init__(self, name = ""):
         
         self.name = name
         self.multiplicity = 1 # La multiplicidad por defecto será 1
 
-    def update_multiplicity(self, multiplicity: int)
+    def update_multiplicity(self, multiplicity: int):
         """ Este método permite modificar la multiplicidad de un arco a un número natural mayor a 0 """
         
         if (multiplicity < 1):
