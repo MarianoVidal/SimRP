46d0fe04a1deef9d9c12636dde86a1fdc4109ed7
MarianoVidal <marianobazalarvidal@gmail.com>
2025-09-14 18:07:30 -0300

    Implementé la clase de Place

---
+++ models/place.py
@@ -0,0 +1,7 @@
+""" Una clase que representa a un Lugar en Redes de Petri """
+
+class Place:
+    """ La clase puede tener un nombre opcionalmente """
+
+    def __init__(self, name = ""):
+        self.name = name
